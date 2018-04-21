f8778a12d88f906b293a30fcb327a616401b4c35
chunchong.wchl <chunlong.wchl@alibaba-inc.com>
2018-04-21 14:53:00 +0800

    多module化

---
+++ jdk-api/src/main/java/com/example/ruzhi/javaapi/StartApp.java
@@ -0,0 +1,31 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.example.ruzhi.javaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author chunlong.wchl
+ * @version $Id: Example.java, v 0.1 2018年04月21日 下午2:20 chunlong.wchl Exp $
+ */
+
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+public class StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+}
